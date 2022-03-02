d6ce52376e3faf699408ff1634f588c0d0f568cd
Hendro <hendrothemail@gmail.com>
2022-03-02 21:51:09 +0700

    Update seeder

--- seeders/seeder.rb
+++ seeders/seeder.rb
@@ -2,6 +2,6 @@ pages << {
   page_type: "listings",
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+  url: "https://www.walmart.com/browse/new-in-movies/0?_be_shelf_id=1522545&facet=shelf_id%3A1522545%7C%7Cbrand%3AWarner+Home+Video%7C%7Cbrand%3ADisney",
   fetch_type: "browser"
 }
