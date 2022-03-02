452da0d5d484733757ae93449db4c5295c2c83d3
Hendro <hendrothemail@gmail.com>
2022-03-03 00:54:01 +0700

    Add parser

---
+++ parsers/listings.rb
@@ -0,0 +1,24 @@
+nokogiri = Nokogiri.HTML(content)
+products = nokogiri.css('div.mid-gray')
+
+products.each do |product|
+  href = product.at_css('a')['href']
+  url = URI.join('https://www.walmart.com', href).to_s
+  pages << {
+      url: url,
+      page_type: 'products',
+      fetch_type: 'browser',
+      vars: {}
+    }
+end
+
+# pagination_links = nokogiri.css('ul.paginator-list li a')
+# pagination_links.each do |link|
+#   url = URI.join('https://www.walmart.com', link['href']).to_s
+#   pages << {
+#       url: url,
+#       page_type: 'listings',
+#       fetch_type: 'browser',
+#       vars: {}
+#     }
+# end
