4ed3e1029f7e86166d3bddc08c62ad6977d28e9c
Hendro <hendrothemail@gmail.com>
2022-03-03 21:09:04 +0700

    Add request headers

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -5,10 +5,20 @@ products.each do |product|
   href = product.at_css('a')['href']
   url = URI.join('https://www.walmart.com', href).to_s
   pages << {
-      url: url,
-      page_type: 'products',
-      fetch_type: 'browser',
-      vars: {}
+		url: url,
+		page_type: 'products',
+		fetch_type: 'browser',,
+		headers: {
+			"User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+			"sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+			"sec-ch-ua-mobile" => "?0",
+			"sec-ch-ua-platform" => "Linux"
+		},
+		display: {
+			"width": 1366,
+			"height": 760
+		},
+		vars: {}
     }
 end
 
@@ -16,9 +26,19 @@ pagination_links = nokogiri.css('nav[aria-label="pagination"] ul li a')
 pagination_links.each do |link|
   url = URI.join('https://www.walmart.com', link['href']).to_s
   pages << {
-      url: url,
-      page_type: 'listings',
-      fetch_type: 'browser',
-      vars: {}
+		url: url,
+		page_type: 'listings',
+		fetch_type: 'browser',
+		headers: {
+			"User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+			"sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+			"sec-ch-ua-mobile" => "?0",
+			"sec-ch-ua-platform" => "Linux"
+		},
+		display: {
+			"width": 1366,
+			"height": 760
+		},
+		vars: {}
     }
 end
--- seeders/seeder.rb
+++ seeders/seeder.rb
@@ -1,7 +1,16 @@
 pages << {
   page_type: "listings",
   method: "GET",
-  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  headers: {
+    "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+    "sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+    "sec-ch-ua-mobile" => "?0",
+    "sec-ch-ua-platform" => "Linux"
+  },
   url: "https://www.walmart.com/browse/movies-tv-shows/tv-shows-blu-ray/4096_6003122_8068379",
-  fetch_type: "browser"
+  fetch_type: "browser",
+  display: {
+    "width": 1366,
+    "height": 760
+  }
 }
