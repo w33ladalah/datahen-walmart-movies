6674687983b70f2c034b661aff0d3750a9d770eb
Hendro <hendrothemail@gmail.com>
2022-03-03 21:45:38 +0700

    Fix listings parser error

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -22,23 +22,23 @@ products.each do |product|
     }
 end
 
-pagination_links = nokogiri.css('nav[aria-label="pagination"] ul li a')
-pagination_links.each do |link|
-  url = URI.join('https://www.walmart.com', link['href']).to_s
-  pages << {
-		url: url,
-		page_type: 'listings',
-		fetch_type: 'browser',
-		headers: {
-			"User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-			"sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-			"sec-ch-ua-mobile" => "?0",
-			"sec-ch-ua-platform" => "Linux"
-		},
-		display: {
-			"width": 1366,
-			"height": 760
-		},
-		vars: {}
-    }
-end
+# pagination_links = nokogiri.css('nav[aria-label="pagination"] ul li a')
+# pagination_links.each do |link|
+#   url = URI.join('https://www.walmart.com', link['href']).to_s
+#   pages << {
+# 		url: url,
+# 		page_type: 'listings',
+# 		fetch_type: 'browser',
+# 		headers: {
+# 			"User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+# 			"sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+# 			"sec-ch-ua-mobile" => "?0",
+# 			"sec-ch-ua-platform" => "Linux"
+# 		},
+# 		display: {
+# 			"width": 1366,
+# 			"height": 760
+# 		},
+# 		vars: {}
+#     }
+# end
