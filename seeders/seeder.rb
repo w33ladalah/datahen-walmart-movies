cbf7f5c86b72aa13d469c7f2f1048ef2c4684ffe
Hendro <hendrothemail@gmail.com>
2022-03-03 19:23:35 +0700

    Update seeder

--- seeders/seeder.rb
+++ seeders/seeder.rb
@@ -3,5 +3,5 @@ pages << {
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   url: "https://www.walmart.com/browse/movies-tv-shows/tv-shows-blu-ray/4096_6003122_8068379",
-  # fetch_type: "browser"
+  fetch_type: "browser"
 }
