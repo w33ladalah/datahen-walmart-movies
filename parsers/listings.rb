3fd7795752ec0f4a9a87178e749d1793074a60f6
Hendro <hendrothemail@gmail.com>
2022-03-03 14:04:58 +0700

    Fix listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,7 +1,5 @@
 nokogiri = Nokogiri.HTML(content)
-products = nokogiri.css('html')
-
-p products
+products = nokogiri.css('div.mid-gray')
 
 products.each do |product|
   href = product.at_css('a')['href']
